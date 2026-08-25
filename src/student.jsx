3b33f9b0923a791b59b882e777b518d0f5fdef32
MarkinoSk <177926858+MarkinoSk@users.noreply.github.com>
2026-08-25 18:48:41 +0200

    propTypes

--- src/student.jsx
+++ src/student.jsx
@@ -1,4 +1,4 @@
-
+import PropTypes from 'prop-types'
 function Student(props){
     return(
         <div className="Student">
@@ -8,4 +8,13 @@ function Student(props){
         </div>
     );
 }
+
+// toto dole je dobre na debbuging ak by v inpute alebo v jednotkach bola chyba ako napr namiesto cisla by bol string tak v console vypise chybu(jednotky musim zmenit v App.jsx)
+
+Student.proptypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    isStudent: PropTypes.bool,
+} 
+
 export default Student
